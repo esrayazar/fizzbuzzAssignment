d163465fd126f9d982447061d904fc5107e370d8
Esra Yazar <esrayzr@gmail.com>
2021-09-21 20:32:17 -0400

    fizzbuzz assignment completed.

---
+++ yazar_esra_fizzbuzz.js
@@ -0,0 +1,17 @@
+// console.log("fizzbuzz")
+for(var i=1; i<=100; i++){
+    // console.log("current i value= ", i)
+    if(i%5==0 && i%3==0){
+        console.log("FizzBuzz")
+    } 
+    else if (i%3==0){
+        console.log("Fizz")
+    }
+    else if (i%5==0){
+        console.log("Buzz")
+    }
+    else{
+        console.log(i)
+    }
+
+}
